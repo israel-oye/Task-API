75b2399de55a83437fd8516ea8794652f0248145
israel-oye <ioyeboade@gmail.com>
2023-12-22 17:09:00 +0100

    Update Task model

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class Task(models.Model):
         ('TBD', 'Not done'),
     ]
 
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
     description = models.CharField(max_length=500, blank=True)
     status = models.CharField(
         max_length=3,
@@ -20,7 +20,7 @@ class Task(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=...)
     due_date = models.DateTimeField()
-    creator = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    creator = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return "<Task: %s>" % self.name[:20]+'...' if len(self.name) > 21 else self.name
